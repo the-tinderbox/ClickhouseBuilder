c84c630f74f5e41e5265f213c238e764fdb9c048
Александр Ярославцев <me@facedsid.ru>
2018-11-17 15:41:11 +0300

    fixed functions tests coverage

--- src/functions.php
+++ src/functions.php
@@ -1,5 +1,49 @@
 <?php
 
+// @codeCoverageIgnoreStart
+if (! function_exists('tap')) {
+    /**
+     * Call the given Closure with the given value then return the value.
+     *
+     * @param mixed    $value
+     * @param callable $callback
+     *
+     * @return mixed
+     */
+    function tap($value, $callback)
+    {
+        $callback($value);
+        
+        return $value;
+    }
+}
+// @codeCoverageIgnoreEnd
+
+// @codeCoverageIgnoreStart
+if (! function_exists('array_flatten')) {
+    /**
+     * Flatten a multi-dimensional array into a single level.
+     *
+     * @param array $array
+     * @param int   $depth
+     *
+     * @return array
+     */
+    function array_flatten($array, $depth = INF): array
+    {
+        return array_reduce($array, function ($result, $item) use ($depth) {
+            if (! is_array($item)) {
+                return array_merge($result, [$item]);
+            } elseif ($depth === 1) {
+                return array_merge($result, array_values($item));
+            } else {
+                return array_merge($result, array_flatten($item, $depth - 1));
+            }
+        }, []);
+    }
+}
+// @codeCoverageIgnoreEnd
+
 if (! function_exists('raw')) {
     /**
      * Wrap string into Expression object for inserting in sql query as is.
